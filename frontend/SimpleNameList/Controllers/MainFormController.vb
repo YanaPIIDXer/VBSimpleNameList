6a432853a87600ca124a642d63208490e9b20113
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-02 06:28:42 +0900

    とりあえずサーバからフェッチしてきたリストを放り込むフィールドを定義

--- frontend/SimpleNameList/Controllers/MainFormController.vb
+++ frontend/SimpleNameList/Controllers/MainFormController.vb
@@ -1,3 +1,13 @@
 ﻿Public Class MainFormController
+    ' リスト
+    Private _personList As List(Of Person) = New List(Of Person)()
+    Public ReadOnly Property PersonList() As List(Of Person)
+        Get
+            Return _personList
+        End Get
+    End Property
 
+    ' リストを取得してくる
+    Public Sub FetchPersonList()
+    End Sub
 End Class
