461bc82d75da6bd690a685d8979264dca46bc110
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-02 06:29:29 +0900

    起動時にリストを取得してくるようにした

--- frontend/SimpleNameList/MainForm.vb
+++ frontend/SimpleNameList/MainForm.vb
@@ -1,3 +1,7 @@
 ﻿Public Class MainForm
     Private _controller As MainFormController = New MainFormController()
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        _controller.FetchPersonList()
+    End Sub
 End Class
