c6f19e682257e92eed06523c5d9f9cf0ce59603a
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-02 05:51:00 +0900

    中身を実装 ※ノーテスト

--- backend/src/app/controllers/person_controller.rb
+++ backend/src/app/controllers/person_controller.rb
@@ -7,11 +7,18 @@ class PersonController < ApplicationController
   def create
     response = { result: false }
 
+    last_name = params[:last_name]
+    first_name = params[:first_name]
+    person = Person.new(last_name: last_name, first_name: first_name)
+    if person.save then
+      response[:result] = true
+    end
+
     render json: response
   end
 
   def read
-    list = []
+    list = Person.all
     response = { list: list }
 
     render json: response
@@ -20,11 +27,27 @@ class PersonController < ApplicationController
   def update
     response = { result: false }
 
+    last_name = params[:last_name]
+    first_name = params[:first_name]
+    person = Person.find_by(id: params[:id])
+    if person != nil then
+      person.first_name = first_name
+      person.last_name = last_name
+      if person.save then
+        response[:result] = true
+      end
+    end
+
     render json: response
   end
 
   def delete
     response = { result: false }
+
+    person = Person.find_by(id: params[:id])
+    if person != nil && person.destroy then
+      response[:result] = true
+    end
 
     render json: response
   end
--- backend/src/config/routes.rb
+++ backend/src/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'person#index'
   post 'person' => 'person#create'
   get 'person' => 'person#read'
-  patch 'person' => 'person#update'
-  delete 'person' => 'person#delete'
+  patch 'person/:id' => 'person#update'
+  delete 'person/:id' => 'person#delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
