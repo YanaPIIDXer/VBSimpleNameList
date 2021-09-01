7ba57104b3024231b1b394c2ac571d9d4429956f
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-02 06:18:59 +0900

    Personクラスを定義

---
+++ frontend/SimpleNameList/Models/Person.vb
@@ -0,0 +1,44 @@
+﻿Public Class Person
+    ' ID
+    Private _id As Integer
+    Public ReadOnly Property Id() As Integer
+        Get
+            Return _id
+        End Get
+    End Property
+
+    ' 苗字
+    Private _lastName As String
+    Public Property LastName() As String
+        Get
+            Return _lastName
+        End Get
+        Set(value As String)
+            _lastName = value
+        End Set
+    End Property
+
+    ' 名前
+    Private _firstName As String
+    Public Property FirstName() As String
+        Get
+            Return _firstName
+        End Get
+        Set(value As String)
+            _firstName = value
+        End Set
+    End Property
+
+    ' コンストラクタ
+    Sub New(id As Integer, lastName As String, firstName As String)
+        _id = id
+        _lastName = lastName
+        _firstName = firstName
+    End Sub
+
+    ' ToString
+    ' 「苗字 + " " + 名前」の形で返す
+    Public Overrides Function ToString() As String
+        Return _lastName + " " + _firstName
+    End Function
+End Class
